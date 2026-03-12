438c9a5a4e33311b5e14cca157482f697d6e256a
sydra-alt <Syahrafipradika@gmail.com>
2026-03-12 21:21:17 +0700

    fix base url

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/",
+  base: "/",  // pastikan sudah "/" bukan "/portofolio/"
   plugins: [react(), tailwindcss()],
 })
